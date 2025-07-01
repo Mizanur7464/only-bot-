f68e9e25d29e4d228bf791f2b4a1944ffafc97d0
Mizanur7464 <mdmizanurrahman1860@gmail.com>
2025-07-02 01:57:19 +0600

    update code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
 TOKEN = "8013081955:AAGDYLLBWwJ8OYllyDa0I3CGXCk8CuVKLpQ"
-WEBAPP_URL = "https://apps-frontend-1ovv-git-main-mizanurs-projects-24e9ba9d.vercel.app/"
+WEBAPP_URL = "https://apps-frontend-git-main-mizanurs-projects-24e9ba9d.vercel.app/"
 
 bot = telebot.TeleBot(TOKEN)
 
@@ -11,5 +11,6 @@ def start(message):
     keyboard = InlineKeyboardMarkup()
     keyboard.add(InlineKeyboardButton("🚀 Open Mini App", web_app=WebAppInfo(url=WEBAPP_URL)))
     bot.send_message(message.chat.id, "Open the mini app by clicking the button below:", reply_markup=keyboard)
-
+ 
 bot.polling() 
+
